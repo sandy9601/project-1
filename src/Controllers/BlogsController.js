23aea59a21050d34957991c21d98ce4811f78317
sandy9601 <darshanamsandeep375@gmail.com>
2022-06-21 21:55:47 +0530

    validations

--- src/Controllers/BlogsController.js
+++ src/Controllers/BlogsController.js
@@ -4,11 +4,27 @@ const BlogModel=require("../Models/BlogModel")
 
 const CreateBlog=async function(req,res){
     try{
-    const data=req.body
-    data =req.body
+    const data =req.body
+    console.log(data)
     if(!data){
-        return res.status(400).send({status:true,msg:"body couldnot be empty"})
+        return res.status(400).send({status:false,msg:"body couldnot be empty"})
     }
+    if(!data.tittle){
+       return res.status(400).send({status:false,msg:"title couldnot be empty"})
+    }
+    //if(!data.author_id){
+      //  return res.status(400).send({status:true,msg:"Author Id required"})
+     //}
+     if(!data.body){
+        return res.status(400).send({status:false ,msg: "body couldnot be empty"})
+     }
+     if(!data.category){
+        return res.status(400).send({status:false ,msg: "write category"})
+     }
+
+
+
+
     const CreateBlog=await BlogModel.create(data)
     return res.status(201).send({status:true,data:CreateBlog})
 }
