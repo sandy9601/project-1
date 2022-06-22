8ccf14997ca6e6cddbcda3ed7ba0c1d12f6bb199
sandy9601 <darshanamsandeep375@gmail.com>
2022-06-22 21:54:28 +0530

    project-1

--- src/Middleware/CommonMiddleware.js
+++ src/Middleware/CommonMiddleware.js
@@ -1,12 +1,8 @@
 const AuthorModel=require("../Models/AuthorModel")
 
+// req body validation
 const mid1=async function(req,res,next){
     try{
-let data =req.body
-        if(Object.keys(data).length===0){
-            return res.status(400).send({status:true,msg:"body couldnot be empty"})
-         }
-
 let author_id=data.author_id
 let author=await AuthorModel.findById(author_id)
 if(!author){
@@ -18,6 +14,5 @@ else{
 catch(err){
     res.status(500).send({status:false,error:err.message})
 }}
-
 
 module.exports.mid1=mid1
